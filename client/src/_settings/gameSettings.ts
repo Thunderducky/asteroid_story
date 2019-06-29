c2ca9759c3f056fe59762d2939f4aa61cd199f47
Eric Scott <scottzillaster@gmail.com>
2019-06-29 17:57:36 -0500

    converted the settings back to normal

--- client/src/_settings/gameSettings.ts
+++ client/src/_settings/gameSettings.ts
@@ -11,15 +11,15 @@
 // }
 
 const SETTINGS = {
-    TILE_WIDTH: 4,     // pixels
-    TILE_HEIGHT: 4,    // pixels
+    TILE_WIDTH: 10,     // pixels
+    TILE_HEIGHT: 10,    // pixels
     MAP_WIDTH: 120,      // everything else is in tiles
     MAP_HEIGHT: 120,
-    CAMERA_WIDTH: 120,
-    CAMERA_HEIGHT: 120,
-    SCREEN_WIDTH : 120,
-    SCREEN_HEIGHT : 120,
-    FOV_RADIUS: 10
+    CAMERA_WIDTH: 50,
+    CAMERA_HEIGHT: 50,
+    SCREEN_WIDTH : 50,
+    SCREEN_HEIGHT : 50,
+    FOV_RADIUS: 25
 }
 
 export default SETTINGS
