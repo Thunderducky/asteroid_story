139569faf8c8f9bdf6c4e48ea0af6b9c39fa13f0
Eric Scott <scottzillaster@gmail.com>
2019-07-05 16:33:40 -0500

    started on some prework, but I want to overhaul our current architecture to make it more like an ECS system

---
+++ client/src/gameData.ts
@@ -0,0 +1,6 @@
+// This is a way for us to request game data, it acts as a big object,
+// and we can control when we initialize all of it
+// which we will split into files later
+const GameData  = {}
+
+export { GameData}
