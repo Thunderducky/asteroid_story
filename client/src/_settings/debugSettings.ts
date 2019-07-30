8e847a149e9fcd849e5a16d1d63d5e10a9119158
Eric Scott <scottzillaster@gmail.com>
2019-07-30 18:25:54 -0500

    debug settings

--- client/src/_settings/debugSettings.ts
+++ client/src/_settings/debugSettings.ts
@@ -8,15 +8,15 @@ interface Hash<T> {
 const DEBUG: Hash<boolean> = {
     DISABLE_FOV: false,          // SHOW EVERYTHING, NO HIDING STUFF BEHIND FOV, EVERYTHING ACTS LIT
     DISABLE_TRACKING: false,     // CAMERA WILL ONLY RESPOND TO DIRECT CONTROL AND WON'T TRACK CHARACTER
-    ENABLE_CLIPPING: true,
+    ENABLE_CLIPPING: false,
     DEBUG_DRAW: false,
     STAGE_MAP_GENERATORS: false, // We will manually progress the level generator,
-    ALWAYS_SHOW_ENTITIES: true,
-    SHOW_UNEXPLORED: true,
+    ALWAYS_SHOW_ENTITIES: false,
+    SHOW_UNEXPLORED: false,
     SHOW_COLOR_BAR: false,
     SKIP_ENTITY_PLACEMENT: false,
     SKIP_ENEMY_TURN: false,
-    PLAYER_UNKILLABLE: true,
+    PLAYER_UNKILLABLE: false,
     MOUNT_WINDOW_DATA: false
 }
 
